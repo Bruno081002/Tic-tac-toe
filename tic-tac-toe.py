f978390300f0776c15a4b0b05b073a28cfd3a5f7
Bruno Fortes <brunofortes2002@gmail.com>
2025-07-21 14:23:02 +0100

    Something to not forget

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -7,6 +7,24 @@ def show_board(board):
     for row in board:
         print("|".join(row))
 
+
+def winner(board, player):
+    for i in range(3):
+        if board[0][i] == player and board[1][i] == player and board[2][i] == player:
+            return True
+
+    for i in range(3):
+        if board[i][0] == player and board[i][1] == player and board[i][2] == player:
+            return True
+
+        if board[0][0] == player and board[2][2] == player and board[2][2] == player:
+            return True
+        if board[0][2] == player and board[2][2] == player and board[2][0] == player:
+            return True
+
+    return False
+
+
 for plays in range(9):
     show_board(board)
     choice = input(f"Next Player: {player}, chose a posiction (1-9):")
@@ -24,4 +42,9 @@ for plays in range(9):
         player = "X"
     else:
         player = "O"
+    
+    if winner(board, player):
+        print(f"The Winner was: {player}")
+        break
+
 
